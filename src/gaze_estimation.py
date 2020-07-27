6d15b3bac7b0e53ac455e9ccba847924d40274af
Yannick Serge <yannicksergeobam@gmail.com>
2020-07-27 19:16:36 +0100

    updating inference pipeline

--- src/gaze_estimation.py
+++ src/gaze_estimation.py
@@ -79,15 +79,15 @@ class GazeEstimation:
 
         center_x_left_eye = int((eyes_coords[0][1][0] - eyes_coords[0][0][0]) / 2 + eyes_coords[0][0][0])
         center_y_left_eye = int((eyes_coords[0][1][1] - eyes_coords[0][0][1]) / 2 + eyes_coords[0][0][1])
-        new_x_left_eye = int(center_x_left_eye + x * 40)
-        new_y_left_eye = int(center_y_left_eye + y * 40 * -1)
-        cv2.line(cropped_face, (center_x_left_eye, center_y_left_eye), (new_x_left_eye, new_y_left_eye), (0, 255, 0), 2)
+        new_x_left_eye = int(center_x_left_eye + x * 90)
+        new_y_left_eye = int(center_y_left_eye + y * 90 * -1)
+        cv2.arrowedLine(cropped_face, (center_x_left_eye, center_y_left_eye), (new_x_left_eye, new_y_left_eye), (0, 255, 0), 2)
 
         center_x_right_eye = int((eyes_coords[1][1][0] - eyes_coords[1][0][0]) / 2 + eyes_coords[1][0][0])
         center_y_right_eye = int((eyes_coords[1][1][1] - eyes_coords[1][0][1]) / 2 + eyes_coords[1][0][1])
-        new_x_right_eye = int(center_x_right_eye + x * 40)
-        new_y_right_eye = int(center_y_right_eye + y * 40 * -1)
-        cv2.line(cropped_face, (center_x_right_eye, center_y_right_eye), (new_x_right_eye, new_y_right_eye),
+        new_x_right_eye = int(center_x_right_eye + x * 90)
+        new_y_right_eye = int(center_y_right_eye + y * 90 * -1)
+        cv2.arrowedLine(cropped_face, (center_x_right_eye, center_y_right_eye), (new_x_right_eye, new_y_right_eye),
                  (0, 255, 0), 2)
 
         return x, y, z
---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\obeshor\desktop\computer-pointer-controller\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
